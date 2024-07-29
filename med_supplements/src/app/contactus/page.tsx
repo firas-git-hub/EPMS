c4e5f8ab628131abb81390ca66149df0a4fdc524
firas <firas.moukhallalaty@gmail.com>
2024-07-30 00:02:13 +0200

    fix attempt

---
+++ med_supplements/src/app/contactus/page.tsx
@@ -0,0 +1,17 @@
+import "./page.scss";
+
+export default function contactUs() {
+    return (
+        <div>
+            <div className="contactus-info">
+                <p className="title"></p>
+                <div className="content">
+                    
+                </div>
+            </div>
+            <div className="contactus-form">
+
+            </div>
+        </div>
+    )
+}
